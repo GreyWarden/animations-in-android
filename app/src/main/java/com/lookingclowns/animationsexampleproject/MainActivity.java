5c9fc36487d50b78581b7d2423a8bd4a8dd64da2
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-05 13:01:46 +0200

    Translation animation

--- app/src/main/java/com/lookingclowns/animationsexampleproject/MainActivity.java
+++ app/src/main/java/com/lookingclowns/animationsexampleproject/MainActivity.java
@@ -1,27 +1,29 @@
 package com.lookingclowns.animationsexampleproject;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private final long FADE_DURATION = 2000;
-    private boolean IS_BART_SHOWN = true;
+    private final long SHORT_ANIMATION_DURATION = 500;
+    private final long MEDIUM_ANIMATION_DURATION = 1000;
+    private final long LONG_ANIMATION_DURATION = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void fadeHandler (View view) {
         ImageView bartView = findViewById(R.id.bartView);
-        ImageView homerView = findViewById(R.id.homerView);
-
-        bartView.animate().alpha(this.IS_BART_SHOWN ? 0 : 1).setDuration(FADE_DURATION);
-        homerView.animate().alpha(this.IS_BART_SHOWN ? 1 : 0).setDuration(FADE_DURATION);
-
-        this.IS_BART_SHOWN = !this.IS_BART_SHOWN;
+        bartView.animate().translationZBy(-1000).setDuration(SHORT_ANIMATION_DURATION);
+        bartView.animate().translationYBy(1000).setDuration(MEDIUM_ANIMATION_DURATION);
+        bartView.animate().translationXBy(-1500).setDuration(LONG_ANIMATION_DURATION);
     }
 }
