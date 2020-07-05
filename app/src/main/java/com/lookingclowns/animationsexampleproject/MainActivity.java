700edad0c0bf24f54bd9d794a53608f0feae9660
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-05 13:21:24 +0200

    Scale animation

--- app/src/main/java/com/lookingclowns/animationsexampleproject/MainActivity.java
+++ app/src/main/java/com/lookingclowns/animationsexampleproject/MainActivity.java
@@ -1,27 +1,27 @@
 package com.lookingclowns.animationsexampleproject;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private final long SHORT_ANIMATION_DURATION = 500;
     private final long MEDIUM_ANIMATION_DURATION = 1000;
-    private final long LONG_ANIMATION_DURATION = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView bartView = findViewById(R.id.bartView);
+        bartView.setX(-1500);
+        bartView.animate().translationXBy(1500).rotation(3600).setDuration(MEDIUM_ANIMATION_DURATION);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void fadeHandler (View view) {
         ImageView bartView = findViewById(R.id.bartView);
-        bartView.animate().rotation(-3600).alpha(0).setDuration(MEDIUM_ANIMATION_DURATION);
+        bartView.animate().translationXBy(-1500).scaleX(0.5f).scaleY(0.5f)
+                .rotation(-3600).alpha(0)
+                .setDuration(MEDIUM_ANIMATION_DURATION);
     }
 }
